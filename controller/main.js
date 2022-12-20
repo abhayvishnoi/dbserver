03634f0c7e44f1eff24dc46690e7bbcb4c297bcd
abhayvishnoi <abhayvishnoi6@gmail.com>
2022-12-21 01:42:06 +0530

    db-server

---
+++ controller/main.js
@@ -0,0 +1,22 @@
+createRows = async (req, res, next) => {
+  res.status(200).json({
+    message: "Create",
+  });
+};
+getRows = async (req, res, next) => {
+  res.status(200).json({
+    message: "Create",
+  });
+};
+updateRows = async (req, res, next) => {
+  res.status(200).json({
+    message: "Create",
+  });
+};
+deleteRows = async (req, res, next) => {
+  res.status(200).json({
+    message: "Create",
+  });
+};
+
+module.exports = { createRows, getRows, updateRows, deleteRows };
